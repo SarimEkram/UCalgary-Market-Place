a0bd58b000eb98dc5c528d8874532990970b07e9
eni balogun <enioluwafe.balogun@ucalgary.ca>
2025-11-04 11:01:12 -0700

    fix:backend container works

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 !!Set Up Instructions 
 
-1. Run 
-
-
+1. docker compose up --build, if having trouble do sudo docker compose up --build 
+1A. Adding the -d option will put all of the messages in a log file, keeping the terminal clean. 
 !! Password Strategy 
+2. Change port numbers, 8080, and 3000 in compose file. 
--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors()); // Allow Vite frontend
 
-
+const port = process.env.PORT || 8080; 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -15,13 +15,22 @@ const db = mysql.createConnection({
   database: process.env.DB_NAME,
 });
 
-const port = process.env.PORT || 8080; 
 
 
-app.get(
-  "/",
-    (req, res, ) => {
-      res.status(200).send({ message: "Hello world!!" });}
-);
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+app.get("/", function(req, res, next) {
+  // Random table to make sure that db connection is working 
+  db.query('SELECT * FROM cats;', function (err, results) {
+    if (err) {
+      console.error("Error executing query:", err);
+      return res.status(500).json({ error: "Error fetching data from database" });
+    }
+    // Return the results as JSON in the response
+    res.json({ message: `Hello from MyWORLD`, cats: results });
+  });
+   
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}\n\n`));
