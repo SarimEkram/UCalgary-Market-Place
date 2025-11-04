12e737affe374125fd0489e2c0e7a7a95f592540
eni balogun <enioluwafe.balogun@ucalgary.ca>
2025-11-04 14:33:08 -0700

    fix: enable hmr for vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const port = process.env.PORT || 3000; 
 export default defineConfig({
   plugins: [react()],
+  server : {
+  port: port, 
+  host: '0.0.0.0',
+  watch: {
+    usePolling: true,
+  },
+}
 })
