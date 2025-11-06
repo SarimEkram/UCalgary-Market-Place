a2a20189140114fd8d18e0a3079730020f9d84d9
eni balogun <enioluwafe.balogun@ucalgary.ca>
2025-11-06 11:20:50 -0700

    doc: delete typo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,7 +33,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={handleClick}>
-          MYYYY count is {count}
+           count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
