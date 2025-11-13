2cc56c92cdef04191bb53214d0c226817ff2d9c9
eni balogun <enioluwafe.balogun@ucalgary.ca>
2025-11-12 17:35:44 -0700

    feat: finish styling the header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,28 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import Logo from "../assets/logo.svg";
 
 export default function Header() {
   return (
-    <Stack id="hello" direction="row" spacing={2} sx={styles.container}>
+    <Stack
+      id="hello"
+      direction="row"
+      spacing={2}
+      sx={(theme) => ({
+        borderBottom: theme.palette.divider.width,
+        borderColor: theme.palette.divider.color,
+        ...styles.container
+      })}
+    >
       <Box
         component="img"
         src={Logo}
         alt="Site Logo: A red price tag with letters (UC) written on it in yellow."
-        sx={styles.tinyLogo}
+        sx={(theme) => ({
+          width: "auto",
+          height: theme.typography.h5.fontSize,
+        })}
       ></Box>
-      <Box component="h2">STUDENT MARKETPLACE</Box>
+      <Typography variant="h5">STUDENT MARKETPLACE</Typography>
     </Stack>
   );
 }
@@ -24,11 +36,6 @@ const styles = {
     paddingBottom: 2,
     paddingTop: 2,
     width: "100%",
+   
   },
-
-  tinyLogo: {
-    width: "auto",
-    height: "1.5rem",
-  },
-  title: {},
 };
