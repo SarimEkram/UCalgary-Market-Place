b909e753595a6dcc3038521ea50485824abaa32b
eni balogun <enioluwafe.balogun@ucalgary.ca>
2025-11-12 22:11:17 -0700

    feat: create styled input field

--- frontend/src/components/InputField.jsx
+++ frontend/src/components/InputField.jsx
@@ -1,6 +1,36 @@
+import { FormControl, FormHelperText, Input, InputLabel } from "@mui/material";
 
-export default function InputField() {
+export default function InputField({
+  autoComplete,
+  label,
+  placeholder,
+  helpText,
+  inputProps
+}) {
   return (
-    <div>InputField</div>
-  )
+    <FormControl
+      required={true}
+      variant="standard"
+      fullWidth
+      autoComplete={autoComplete}
+    >
+      <InputLabel
+        shrink
+        htmlFor={label + "-input"}
+      >
+        {label}
+      </InputLabel>
+      <Input {...inputProps} placeholder={placeholder} id={label + "-input"} aria-describedby={label + "-helper-text"} />
+      <FormHelperText
+        disabled
+        sx={{
+          "&.Mui-disabled": { visibility: "hidden" },
+          "&.Mui-error": { visibility: "initial" },
+        }}
+        id={label + "-helper-text"}
+      >
+        *Required field. {helpText}
+      </FormHelperText>
+    </FormControl>
+  );
 }
