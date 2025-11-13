d28e6fe9852b113eb80fad0815fdf7cf4205f887
eni balogun <enioluwafe.balogun@ucalgary.ca>
2025-11-12 18:17:56 -0700

    feat: add layout styling

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,3 +1,4 @@
+import { Stack } from "@mui/material";
 import CustomButton from "./components/CustomButton";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
@@ -5,10 +6,17 @@ import Navigation from "./components/Navigation";
 
 export default function Login() {
   return (
-    <>
-   <Header></Header>
-   <CustomButton>this is a button</CustomButton>
-   <Navigation></Navigation>
-   </>
+    <Stack id="login" direction="column" sx={styles.main}>
+    <Header></Header>
+    <CustomButton>this is a button</CustomButton>
+    <Navigation></Navigation>
+  </Stack>
   )
+}
+
+const styles = {
+  main: {
+   minHeight: "100vh", 
+   justifyContent: "space-between"
+  }
 }
