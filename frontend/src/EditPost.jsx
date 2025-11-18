98267140b5d027070b0a6af35e08465ecef66b8a
enibalo <enioluwafe.balogun@ucalgary.ca>
2025-11-17 23:10:19 -0700

    feat: add missing input fields, and fetching logic

--- frontend/src/EditPost.jsx
+++ frontend/src/EditPost.jsx
@@ -3,17 +3,22 @@ import {
   Container,
   FormHelperText,
   Stack,
-  ToggleButtonGroup,
   ToggleButton,
+  ToggleButtonGroup,
+  Link,
+  Divider,
+  Typography,
+  Input,
 } from "@mui/material";
-import { useState } from "react";
+import { useTheme } from "@mui/material/styles";
+import { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import ProfileIcon from "./assets/ProfileIconSVG";
 import CustomButton from "./components/CustomButton";
 import Header from "./components/Header";
 import InputField from "./components/InputField";
-
-// 1 Backend Tasks (Ctrl+F "BTASK")
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+// 2 Backend Tasks (Ctrl+F "BTASK")
 export default function MyPosts() {
   const {
     register,
@@ -22,23 +27,40 @@ export default function MyPosts() {
   } = useForm();
 
   const [editFailed, setEditFailed] = useState(false);
+  const [deletedImages, setDeletedImages] = useState([]);
+  const [newImages, setNewImages] = useState([]);
+
+  //set the current image in image slider
+  const [currentImage, setCurrentImage] = useState(null);
+
+  const fileInputRef = useRef(null);
+// TODO: find  a way to handle deleted images 
   const onSubmit = (data) => {
     data["condition"] = condition;
-    console.log("send edit post request to the server... using this data:", data);
+    data["deleted_images"] = deletedImages;
+    data["new_images"] = Array.from(newImages);
+    console.log(
+      "send edit post request to the server... using this data:",
+      data
+    );
     /**
      * 
      BTASK
      ------
-     Validating a login attempt.
-     Setting the variable `success` based on the results.   
+     Updated an edit post.    
 
      Example Data
      --------
-     {
-    "email": "enibalo2@gmail.com",
-    "password": "butter123#"
-    
-    }
+    {
+    "name": "eni",
+    "description": "rni",
+    "location": "t3a2m1",
+    "price": 13,
+    "condition": "new",
+    "deleted_images": [image_id1, imageid_2...etc],
+    "new_images": [Fileobject, Fileobject  ]
+}
+}
      */
 
     const success = true;
@@ -50,15 +72,86 @@ export default function MyPosts() {
   };
 
   const [condition, setCondition] = useState("new");
-  const handleConditionChange = (value) => {
-    setCondition(value);
+  const handleConditionChange = (event, value) => {
+    if (value != null) {
+      setCondition(value);
+    }
+    console.log(condition);
   };
+
+  function handleDeletedImage() {
+    let imgs = Array.from(deletedImages);
+    imgs.push(currentImage);
+    setDeletedImages(imgs);
+    console.log("delete image using this data...", data);
+  }
+
+  const handleImagesChange = (event) => {
+    const newFiles = event.target.files;
+    if (newFiles.length != 0) {
+      setNewImages(newFiles);
+    }
+  };
+
+  const printImageNames = (files) => {
+    let result = "";
+    console.log("files", files);
+    for (let i = 0; i < files.length; i++) {
+      result += (result != "" ? ", " : "") + files[i].name;
+    }
+
+    return result;
+  };
+
+  //TODO: finish getPost
+  const [name, setName] = useState(""); 
+  const [location, setLocation] = useState("");
+  const getPost = ()=>{
+    /**
+     * BTASK 
+     * GET POST
+     * ----- 
+     * USER ID: 
+     * POST ID: 
+     * 
+     {
+     user_id:1234 
+      post_id: 45678
+      }
+     * 
+     */
+
+  }
 
   return (
     <Stack id="login" direction="column" spacing={2} sx={styles.page}>
       <Header></Header>
       <Container maxWidth={"sm"} sx={styles.main}>
-        
+        <Link
+          color="secondary"
+          sx={{
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            textDecoration: "none",
+          }}
+          variant="text"
+        >
+          <ChevronLeftIcon></ChevronLeftIcon>
+          <Typography variant="h6" sx={{ fontWeight: "400" }}>
+            Back to My Posts
+          </Typography>
+        </Link>
+        <Divider
+          variant="fullWidth"
+          sx={(theme) => ({
+            boxSizing: "border-box",
+            borderBottom: theme.palette.dividerWidth,
+            borderColor: theme.palette.divider,
+            marginTop: 3,
+            marginBottom: 3,
+          })}
+        ></Divider>
         <form onSubmit={handleSubmit(onSubmit)} noValidate>
           <Stack direction="column" component={"div"} spacing={4}>
             <InputField
@@ -108,26 +201,99 @@ export default function MyPosts() {
               })}
             ></InputField>
             <InputField
-              placeholder={"Ex: 15.00/hr, 15.00/session, 20.00"}
+              placeholder={"15.00"}
               label={"Price"}
+              type="number"
               errorMsg={errors["price"] ? errors["price"].message : null}
               {...register("price", {
                 required: "Price is required.",
+                valueAsNumber : true
               })}
             ></InputField>
             <ToggleButtonGroup
-              color="secondary"
+              id="condition-toggle"
               value={condition}
               exclusive
               onChange={handleConditionChange}
+              sx={{
+                "& .MuiToggleButtonGroup-root": {
+                  color: "#000509",
+                  backgroundColor: "#F0F0F3",
+                  textTransform: "none",
+                  fontWeight: "400",
+                  lineHeight: 0.7,
+                },
+
+                "& .MuiButtonBase-root": {
+                  color: "#000509",
+                  backgroundColor: "#F0F0F3",
+                  textTransform: "none",
+                  fontWeight: "400",
+                  lineHeight: 0.7,
+                },
+              }}
             >
-              <ToggleButton value="new">New</ToggleButton>
-              <ToggleButton value="good">Good</ToggleButton>
-              <ToggleButton value="fair">Fair</ToggleButton>
+              <ToggleButton value="new" data-selected={condition == "new"}>
+                New
+              </ToggleButton>
+              <ToggleButton value="good" data-selected={condition == "good"}>
+                Good
+              </ToggleButton>
+              <ToggleButton value="fair" data-selected={condition == "fair"}>
+                Fair
+              </ToggleButton>
             </ToggleButtonGroup>
-             <CustomButton color="black" type="submit">Add Image</CustomButton>
-            <CustomButton type="submit">Edit</CustomButton>
+            <Box id="edit-images" sx={{ position: "relative" }}>
+              {/* temporary placeholder until the image slider is done */}
+              <div style={{ backgroundColor: "grey" }}>
+                <div style={{ visibility: "hidden" }}>
+                  <ProfileIcon></ProfileIcon>
+                </div>
+              </div>
+              <CustomButton
+                style={{
+                  width: "fit-content",
+                  position: "absolute",
+                  top: 0,
+                  right: 0,
+                  margin: 2,
+                }}
+                color={"red"}
+                onClick={handleDeletedImage}
+              >
+                {"Delete"}
+              </CustomButton>
+            </Box>
+            <Stack spacing={3}>
+              <CustomButton
+                color="black"
+                onClick={() => {
+                  fileInputRef.current.children[0].click();
+                }}
+              >
+                Add Image
+              </CustomButton>
+
+              <Typography
+                id="star"
+                sx={[{ fontSize: "1rem", visibility: newImages.length != 0 }]}
+              >
+                Selected files:{" "}
+                {newImages.length != 0 && printImageNames(newImages)}
+              </Typography>
+
+              <CustomButton type="submit">Edit</CustomButton>
+            </Stack>
           </Stack>
+          <Input
+            ref={fileInputRef}
+            type="file"
+            accept="image/*"
+            sx={{ display: "none" }}
+            inputProps={{ multiple: true }}
+            onChange={handleImagesChange}
+            disableUnderline
+          ></Input>
         </form>
         <Stack spacing={2} sx={styles.bottomContent}>
           <FormHelperText
@@ -162,6 +328,7 @@ const styles = {
     justifyContent: "center",
     alignContent: "space-between",
     padding: 10,
+    paddingTop: 3,
   },
 
   bottomContent: {
