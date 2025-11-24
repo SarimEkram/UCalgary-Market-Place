b34da289d718483e2e066484f78ea9561ef1a932
Sarim Ekram <sarim.ekram1@ucalgary.ca>
2025-11-24 02:57:20 -0700

    Completed route for settings

--- backend/src/routes/userSettingsRoute/mySettingsRoute.js
+++ backend/src/routes/userSettingsRoute/mySettingsRoute.js
@@ -1,10 +1,6 @@
 import { Router } from "express";
-import { updateUserInfo } from "../../controller/userSettingsController/settingsController.js";
+import { updateUserInfo } from "../../controller/userSettingsController/mySettingsController.js";
 
 const router = Router();
-
-// Update user info (first name, last name, password).
-// Frontend will call: PUT /api/settings
-router.put("/", updateUserInfo);
-
+router.post("/update", updateUserInfo);
 export default router;
