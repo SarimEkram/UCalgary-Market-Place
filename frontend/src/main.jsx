9874b40d0257a91a2b26b42cbc726a303c9e00a6
deepshikhadhammi <deepshikhadhammi@gmail.com>
2025-11-26 14:14:33 -0700

    refactor: move inline styles into styles object & clean up fetch logic

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,6 +13,7 @@ import EditEvent from "./pages/EditEvent";
 import CreateEvent from "./pages/CreateEvent";
 import MySettings from "./pages/MySettings";
 import MarketItemPage from "./pages/MarketItemPage";
+import Event from "./pages/Event";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -134,6 +135,7 @@ createRoot(document.getElementById("root")).render(
           <Route path="home" element={<Home></Home>}></Route>
           <Route path="market" element={<Market></Market>}></Route>
           <Route path="/market/:id" element={<MarketItemPage />} />
+          <Route path="event" element={<Event></Event>}></Route>
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
