90f1fd917269b3508346d92efb9b5052c4bc0a19
Imdad Goraho <imdad.goraho@ucalgary.ca>
2025-11-28 22:05:27 -0700

    fix: reverted accidental changes

--- backend/src/routes/userSettingsRoute/myContactedRoute.js
+++ backend/src/routes/userSettingsRoute/myContactedRoute.js
@@ -1,15 +1,9 @@
 import { Router } from "express";
-import { 
-    getContactedPosts,
-    contactSeller,
-    canContactSeller
-} from "../../controller/userSettingsController/myContactedController.js";
+import { getContactedPosts } from "../../controller/userSettingsController/myContactedController.js";
 
 const router = Router();
 
-// POST /api/getContactedPosts - Get all contacted posts
+// POST /api/getContactedPosts
 router.post("/", getContactedPosts);
 
-// Export router and individual functions for use in app.js
 export default router;
-export { contactSeller, canContactSeller };
