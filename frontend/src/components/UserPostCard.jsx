7bc7755ff22dec1c6038948f8c3c4327f6754b67
enibalo <enioluwafe.balogun@ucalgary.ca>
2025-11-28 17:07:53 -0700

    feat: add more props, and optional components

--- frontend/src/components/UserPostCard.jsx
+++ frontend/src/components/UserPostCard.jsx
@@ -1,45 +1,130 @@
 import {
-    Box, Card, CardActionArea,
-    CardContent,
-    CardMedia, Stack, Typography
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Stack,
+  Typography,
 } from "@mui/material";
-
+import { useNavigate } from "react-router";
 
 export default function PostCard({
   key,
+  link,
   image,
   primaryText,
   secondaryText,
   tertiaryText,
   TopLeftAction,
+  disableNavigation = false, //for cards that shouldn't navigate to the post details page when clicked
 }) {
-  return (
-    <Card
-      variant="outlined"
-      raised={false}
-      square
-      key={key}
-      sx={(theme) => ({
-        boxSizing: "border-box",
-        padding: 3,
-        overflow: "hidden",
-        border: "0px",
-        borderTop: 0.75,
-        borderColor: theme.palette.divider,
-        fontWeight: 0, 
-      })}
-    >
-      <Stack spacing={2} direction="row">
-        <CardActionArea>
+  const navigate = useNavigate();
+  if (disableNavigation == false) {
+    return (
+      <Card
+        variant="outlined"
+        raised={false}
+        square
+        key={key}
+        sx={(theme) => ({
+          boxSizing: "border-box",
+          padding: 3,
+          overflow: "hidden",
+          border: "0px",
+          borderTop: 0.75,
+          borderColor: theme.palette.divider,
+          fontWeight: 0,
+        })}
+      >
+        <Stack spacing={2} direction="row">
+          <CardActionArea onClick={() => navigate(link)}>
+            {/* IMAGE */}
+            <CardContent sx={{ px: 0 }}>
+              <Stack
+                direction={"row"}
+                spacing={2}
+                sx={{ alignItems: "flex-start" }}
+              >
+                <Box sx={{ flexGrow: 1 }}>
+                  <Typography
+                    variant="body1"
+                    sx={{
+                      mt: 0,
+                    }}
+                  >
+                    {primaryText}
+                  </Typography>
+
+                  <Typography
+                    variant="subtitle1"
+                    color="text.secondary"
+                    sx={{}}
+                    noWrap
+                  >
+                    {secondaryText}
+                  </Typography>
+
+                  <Typography
+                    variant="subtitle1"
+                    color="text.secondary"
+                    sx={{
+                      fontSize: {
+                        display: "block",
+                      },
+                    }}
+                    noWrap
+                  >
+                    {tertiaryText}
+                  </Typography>
+                </Box>
+                <TopLeftAction sx={{ flexGrow: 0 }}></TopLeftAction>
+              </Stack>
+            </CardContent>
+            <CardMedia
+              sx={(theme) => ({
+                width: "100%",
+                height: { xs: 220, sm: 260, md: 320 },
+                objectFit: "cover",
+                borderRadius: theme.shape.borderRadius,
+              })}
+              component="img"
+              image={image} //image src
+            />
+          </CardActionArea>
+        </Stack>
+      </Card>
+    );
+  } else {
+    return (
+      <Card
+        variant="outlined"
+        raised={false}
+        square
+        key={key}
+        sx={(theme) => ({
+          boxSizing: "border-box",
+          padding: 3,
+          overflow: "hidden",
+          border: "0px",
+          borderTop: 0.75,
+          borderColor: theme.palette.divider,
+          fontWeight: 0,
+        })}
+      >
+        <Box>
           {/* IMAGE */}
-          <CardContent sx={{px: 0}}>
-            <Stack direction={"row"} spacing={2} sx={{  alignItems: "flex-start"}}>
+          <CardContent sx={{ px: 0 }}>
+            <Stack
+              direction={"row"}
+              spacing={2}
+              sx={{ alignItems: "flex-start" }}
+            >
               <Box sx={{ flexGrow: 1 }}>
                 <Typography
                   variant="body1"
                   sx={{
                     mt: 0,
-                    // fontSize: { xs: "0.8rem", md: "0.9rem" },
                   }}
                 >
                   {primaryText}
@@ -48,9 +133,7 @@ export default function PostCard({
                 <Typography
                   variant="subtitle1"
                   color="text.secondary"
-                  sx={{
-                    // fontSize: { xs: "0.7rem", md: "0.75rem", display: "block" },
-                  }}
+                  sx={{}}
                   noWrap
                 >
                   {secondaryText}
@@ -61,8 +144,6 @@ export default function PostCard({
                   color="text.secondary"
                   sx={{
                     fontSize: {
-                      // xs: "0.7rem",
-                      // md: "0.75rem",
                       display: "block",
                     },
                   }}
@@ -71,21 +152,21 @@ export default function PostCard({
                   {tertiaryText}
                 </Typography>
               </Box>
-              <TopLeftAction style={{ flexGrow: 0 }}></TopLeftAction>
+              <TopLeftAction sx={{ flexGrow: 0 }}></TopLeftAction>
             </Stack>
           </CardContent>
           <CardMedia
-            sx={(theme)=>({
+            sx={(theme) => ({
               width: "100%",
               height: { xs: 220, sm: 260, md: 320 },
               objectFit: "cover",
-              borderRadius: theme.shape.borderRadius, 
+              borderRadius: theme.shape.borderRadius,
             })}
             component="img"
             image={image} //image src
           />
-        </CardActionArea>
-      </Stack>
-    </Card>
-  );
+        </Box>
+      </Card>
+    );
+  }
 }
