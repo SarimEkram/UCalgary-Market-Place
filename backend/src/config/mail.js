08cf458e8756aa8f23d9443d7010488b8b24107c
Sarim Ekram <sarim.ekram1@ucalgary.ca>
2025-12-01 02:21:33 -0700

    changed a comment

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -7,12 +7,11 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-        user: process.env.EMAIL_USER, // same as you already use
-        pass: process.env.EMAIL_PASS, // your Gmail app password
+        user: process.env.EMAIL_USER, // email we are using
+        pass: process.env.EMAIL_PASS, //  Gmail app password
     },
 });
 
-// Optional: verify once on startup
 transporter.verify((err, success) => {
     if (err) {
         console.error("Error setting up mail transporter:", err);
