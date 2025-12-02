7f2f567ad6bc53b1a92e5a593865f6a080bbe6c3
enibalo <enioluwafe.balogun@ucalgary.ca>
2025-12-01 22:36:47 -0700

    feat: connect admin profile to backend

--- frontend/src/pages/AdminProfile.jsx
+++ frontend/src/pages/AdminProfile.jsx
@@ -16,35 +16,21 @@ export default function AdminProfile() {
   useEffect(() => {
     let isMounted = true;
     async function fetchData() {
-      // TODO: BTASK
-      // add fetch request to get admin info (fname, lname, email),all actions:
-      // body of request:
-      // {
-      // id: id
-      // }
+      const response = await fetch(
+        `http://localhost:8080/api/admin/users/${id}/profile`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
-      /* const response = await fetch(
-         `http://localhost:8080/api/something/${id}`,
-         {
-           method: "GET",
-           headers: {
-             "Content-Type": "application/json",
-           },
-         }
-       );
+      let data = await response.json();
 
-       const data = await response.json();
-      */
+      setUserData({ fname: data.admin.fname, lname: data.admin.lname, email: data.admin.email });
 
-      setUserData({ fname: "Jone", lname: "Carter", email: "jw@ucalgary.ca" });
-      let actionData = [
-        { date: "2025-11-01T00:00:00.000Z", action: "Delete a user" },
-        { date: "2025-11-01T00:00:00.000Z", action: "Delete a user" },
-         { date: "2025-11-01T00:00:00.000Z", action: "Delete a user" },
-        { date: "2025-11-01T00:00:00.000Z", action: "Delete a user" },
-         { date: "2025-11-01T00:00:00.000Z", action: "Delete a user" },
-        { date: "2025-11-01T00:00:00.000Z", action: "Delete a user" },
-      ];
+      let actionData = [...data.actions];
       actionData = actionData.map((item) => {
         let dateObject = dayjs(item.date);
 
@@ -97,10 +83,8 @@ export default function AdminProfile() {
   );
 
   return (
-   
-    
-    //1. Root container must look like this 
-  <Stack
+    //1. Root container must look like this
+    <Stack
       direction="row"
       sx={{ bgcolor: "background.paper", minHeight: "100vh" }}
     >
@@ -108,73 +92,73 @@ export default function AdminProfile() {
       <DesktopNav></DesktopNav>
       {/*3. Enclouse Header, and MAIN PAGECONTENT  with this box */}
       <Box sx={{ flex: "1", m: 0 }}>
-      <Header></Header>
-      
-      <Container
-        maxWidth="lg"
-        sx={{
-          flexGrow: 1,
-          py: { xs: 2, md: 4 },
-          px: { xs: 2, sm: 3, md: 6 },
-          display: "flex",
-          flexDirection: "column",
-          gap: { xs: 3, md: 4 },
-          /* 
+        <Header></Header>
+
+        <Container
+          maxWidth="lg"
+          sx={{
+            flexGrow: 1,
+            py: { xs: 2, md: 4 },
+            px: { xs: 2, sm: 3, md: 6 },
+            display: "flex",
+            flexDirection: "column",
+            gap: { xs: 3, md: 4 },
+            /* 
           4: To prevent the bottom content from being covered by the mobile nav
           you may need to adjust the margin forr the Container for your main page content
           */
-          mb: 10,
-        }}
-      >
-        <Box sx={{ borderWidth: 0 }}>
-          <Typography variant="h4">Admin Profile</Typography>
-          <CustomDivider></CustomDivider>
-        </Box>
-        <Box
-          sx={(theme) => ({
-            borderStyle: "solid",
-            borderWidth: 0.75,
-            borderColor: theme.palette.divider,
-            p: 5,
-            borderRadius: theme.shape.borderRadius,
-          })}
+            mb: 10,
+          }}
         >
-          <Stack direction="row" spacing={2} sx={{ alignItems: "center" }}>
-            <ProfileIcon></ProfileIcon>
+          <Box sx={{ borderWidth: 0 }}>
+            <Typography variant="h4">Admin Profile</Typography>
+            <CustomDivider></CustomDivider>
+          </Box>
+          <Box
+            sx={(theme) => ({
+              borderStyle: "solid",
+              borderWidth: 0.75,
+              borderColor: theme.palette.divider,
+              p: 5,
+              borderRadius: theme.shape.borderRadius,
+            })}
+          >
+            <Stack direction="row" spacing={2} sx={{ alignItems: "center" }}>
+              <ProfileIcon></ProfileIcon>
 
-            <Box>
-              <Typography variant="h4">
-                {userData.fname} {userData.lname}
-              </Typography>
-              <Typography variant="h6">{userData.email}</Typography>
-            </Box>
-          </Stack>
-        </Box>
-        <Box
-          sx={(theme) => ({
-            borderStyle: "solid",
-            borderWidth: 0.75,
-            borderColor: theme.palette.divider,
-            p: 5,
-            borderRadius: theme.shape.borderRadius,
-          })}
-        >
-          <Typography variant="h4" sx={{ paddingBottom: 3 }}>
-            Recent Actions:
-          </Typography>
-          {items.map((item, index) => (
-            <ActionItem
-              key={"action" + index}
-              index={index}
-              dateTime={item.dateTime}
-              action={item.action}
-            ></ActionItem>
-          ))}
-        </Box>
-      </Container>
+              <Box>
+                <Typography variant="h4">
+                  {userData.fname} {userData.lname}
+                </Typography>
+                <Typography variant="h6">{userData.email}</Typography>
+              </Box>
+            </Stack>
+          </Box>
+          <Box
+            sx={(theme) => ({
+              borderStyle: "solid",
+              borderWidth: 0.75,
+              borderColor: theme.palette.divider,
+              p: 5,
+              borderRadius: theme.shape.borderRadius,
+            })}
+          >
+            <Typography variant="h4" sx={{ paddingBottom: 3 }}>
+              Recent Actions:
+            </Typography>
+            {items.map((item, index) => (
+              <ActionItem
+                key={"action" + index}
+                index={index}
+                dateTime={item.dateTime}
+                action={item.action}
+              ></ActionItem>
+            ))}
+          </Box>
+        </Container>
       </Box>
       {/* 5: Place the mobile nav after the box  */}
-     <MobileNav></MobileNav>
+      <MobileNav></MobileNav>
     </Stack>
   );
 }
