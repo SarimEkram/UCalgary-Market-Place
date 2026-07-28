2adb8873c2264051e2fa52ce0890508ef78340ab
Sarim Ekram <sarim.ekram1@ucalgary.ca>
2026-07-28 11:59:29 +0600

    feat: add helmet security headers and 1MB body size limit

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
 
 import { requireAuth, requireAdmin } from "./middleware/auth.js";
 import loginRoutes from "./routes/authRoutes/loginRoutes.js";
@@ -29,6 +30,8 @@ import getRecentActionsRoutes from "./routes/adminRoutes/getRecentActionsRoute.j
 
 
 const app = express();
+app.use(helmet());
+app.use(express.json({ limit: "1mb" }));
 
 app.use(express.json());
 app.use(cookieParser());
